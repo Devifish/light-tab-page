2568780637d417e4dce757f08cc1be7b7d0ccf1d
zhangyidong <devifish@outlook.com>
2023-02-24 15:42:43 +0800

    update: 更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -10,22 +10,21 @@ export default defineConfig(env => ({
   envPrefix: ["APP_", "npm_package_name", "npm_package_version"],
   resolve: {
     alias: {
-      "@/": `${resolve(__dirname, "src")}/`
+      "@": resolve(__dirname, "src")
     }
   },
   build: {
     minify: env.mode !== "development",
-    brotliSize: false,
     chunkSizeWarningLimit: 1024,
     rollupOptions: {
       output: {
-        manualChunks: getManualChunks({
+        manualChunks: {
           "ant-design": ["ant-design-vue", "@ant-design/icons-vue"]
-        }),
+        },
         chunkFileNames: "js/[name]-[hash].js",
         entryFileNames: "js/[name]-[hash].js",
         assetFileNames: info =>
-          extname(info.name) === ".css"
+          extname(info.name!) === ".css"
             ? "css/[name]-[hash][extname]"
             : "assets/[name]-[hash][extname]"
       }
@@ -58,20 +57,3 @@ export default defineConfig(env => ({
     viteBuildManifest()
   ]
 }))
-
-function getManualChunks(data: Record<string, string[]>) {
-  return (id: string) => {
-    if (!id.includes("node_modules")) return undefined
-
-    const temp = id.split("node_modules/")
-    const relativePath = temp[temp.length - 1] // 兼容pnpm
-    for (let key in data) {
-      for (let lib of data[key]) {
-        if (relativePath.startsWith(`${lib}/`)) {
-          return key
-        }
-      }
-    }
-    return "vendor"
-  }
-}
